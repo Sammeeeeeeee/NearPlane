cbbf606b76adfd76751fab4a5bb962ed91972b6f
Sammeeeeeeee <s.patcas18@gmail.com>
2025-09-25 22:22:43 +0100

    Correct project naming in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -128,7 +128,7 @@ export default function App() {
   return (
     <div className="app">
       <header className="topbar">
-        <h1>Nearest Plane — Live</h1>
+        <h1>NearPlane/h1>
         <small>Status: {status}</small>
       </header>
 
